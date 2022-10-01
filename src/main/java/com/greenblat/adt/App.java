a8c32cb758a8274285a3bcfd1e5315a39a74509d
greenblat17 <leftemail17@mail.ru>
2022-10-01 11:56:58 +0300

    initial commit

---
+++ src/main/java/com/greenblat/adt/App.java
@@ -0,0 +1,13 @@
+package com.greenblat.adt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
